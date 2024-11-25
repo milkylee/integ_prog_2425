7e0cb64483c656e55e858e364d85e26a48a74bd7
Milky Lorejo <milkylorejo@gmail.com>
2024-11-25 22:28:02 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+$(document).on("click", "li span", function(){
+	$(this).parent().fadeOut(function(){
+		$(this).remove();
+	});
+})
+
+$("input[type='text']").on("keypress", function(){
+	if(event.which == 13){
+		var newItem = "<li><span><i class='fa fa-trash-o'></i></span>" + $(this).val() + "</li>";
+		$("ul").append(newItem);
+		$(this).val("");
+	}
+});
+
+$("h1 i").click(function(){
+	$(".slide").slideToggle();
+});
